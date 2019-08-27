c31c5547e8e228457b07e44e25c30dcc04fa9e03
Neil Winton <nwinton@pivotal.io>
2019-08-27 15:34:16 +0530

    Controller unit tests all pass - no web annotations

--- src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -1,31 +1,46 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
 public class TimeEntryController {
-    public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+    private TimeEntryRepository repository;
 
+    public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+        this.repository = timeEntryRepository;
     }
 
     public ResponseEntity<TimeEntry> create(TimeEntry timeEntryToCreate) {
-        return null;
+        TimeEntry result = repository.create(timeEntryToCreate);
+        return new ResponseEntity<TimeEntry>(result, HttpStatus.CREATED);
     }
 
     public ResponseEntity<TimeEntry> read(long timeEntryId) {
-        return null;
+        TimeEntry foundEntry = repository.find(timeEntryId);
+        if (foundEntry == null) {
+            return new ResponseEntity<TimeEntry>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<TimeEntry>(foundEntry, HttpStatus.OK);
+        }
     }
 
     public ResponseEntity<List<TimeEntry>> list() {
-        return null;
+        return new ResponseEntity<List<TimeEntry>>(repository.list(), HttpStatus.OK);
     }
 
-    public ResponseEntity<TimeEntry> update(long timeEntryId, TimeEntry expected) {
-        return null;
+    public ResponseEntity<TimeEntry> update(long timeEntryId, TimeEntry timeEntry) {
+        TimeEntry updatedEntry = repository.update(timeEntryId, timeEntry);
+        if (updatedEntry == null) {
+            return new ResponseEntity<TimeEntry>(HttpStatus.NOT_FOUND);
+        } else {
+            return new ResponseEntity<TimeEntry>(updatedEntry, HttpStatus.OK);
+        }
     }
 
     public ResponseEntity delete(long timeEntryId) {
-        return null;
+        repository.delete(timeEntryId);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
